c0724f62b5f3613671fb9c491446a2271049dd3a
gksdudrb922 <gksdudrb922@gmail.com>
2022-10-04 15:10:06 +0900

    Update Eratosthenes code

--- Eratosthenes.py
+++ Eratosthenes.py
@@ -2,7 +2,7 @@
 import math
 
 n = int(input())
-is_prime = [True] * (n + 1)
+is_prime = [False, False] + [True] * (n - 1)
 for i in range(2, int(math.sqrt(n)) + 1):
     if is_prime[i]:
         for j in range(i * 2, n + 1, i):
