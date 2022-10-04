08a82bf07d5d3f0afd381cb8b21ae6a56adbb789
gksdudrb922 <gksdudrb922@gmail.com>
2022-10-04 14:56:44 +0900

    Add Eratosthenes code

---
+++ Eratosthenes.py
@@ -0,0 +1,12 @@
+# 에라토스테네스의 체 - n 이하 소수 구하기
+import math
+
+n = int(input())
+is_prime = [True] * (n + 1)
+for i in range(2, int(math.sqrt(n)) + 1):
+    if is_prime[i]:
+        for j in range(i * 2, n + 1, i):
+            is_prime[j] = False
+
+primes = [i for i in range(2, n + 1) if is_prime[i]]
+print(primes)
